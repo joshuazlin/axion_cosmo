71ff08d4b9fb60f4468f5823803afffeed9af882
Zhiquan Sun <zqsun@mit.edu>
2023-04-12 18:24:01 -0400

    should be working now

--- src/axion_cosmo/thermal.py
+++ src/axion_cosmo/thermal.py
@@ -33,6 +33,12 @@ def t(eta,t1):
     """
     return (eta**2)*(t1)
 
+def t_T(gstar,T):
+    """
+    (S45)
+    """
+    return 0.3012*(gstar**(-1/2))*1.220890e22/(T**2)
+
 def T(gstar,t):
     """
     S(45)
@@ -49,115 +55,137 @@ def init_PQ_params(fa,gstar):
     t1 = t_T(gstar,T1)
     Tc = 1.68e-7*((400))
     return R1,T1,t1
+
 ''''''
 
 
 def stand_dev_phi(ksq,V,T,fa):
-	'''
-	wrapper function to compute standard deviation of the field given wave number k
-	taking lambda to 1 as mentioned in the paper
-	'''
+    '''
+    wrapper function to compute standard deviation of the field given wave number k
+    taking lambda to 1 as mentioned in the paper
+    '''
 
-	meff2 = T^2 / 3. - fa^2
-	omegak = np.sqrt(ksq + meff2)
-	nk = 1./(np.exp(omegak/T) - 1)
+    meff2 = T**2 / 3. - fa**2
+    omegak = np.sqrt(ksq + meff2)
+    nk = 1./(np.exp(omegak/T) - 1)
 
-	return nk / omegak * V
+    return nk / omegak * V
 
 def stand_dev_phidot(ksq,V,T,fa):
-	'''
-	wrapper function to compute standard deviation of the time derivative of the field given wave number k
-	taking lambda to 1 as mentioned in the paper
-	'''
-	meff2 = T^2 / 3. - fa^2
-	omegak = np.sqrt(ksq + meff2)
-	nk = 1./(np.exp(omegak/T) - 1)
+    '''
+    wrapper function to compute standard deviation of the time derivative of the field given wave number k
+    taking lambda to 1 as mentioned in the paper
+    '''
+    meff2 = T**2 / 3. - fa**2
+    omegak = np.sqrt(ksq + meff2)
+    nk = 1./(np.exp(omegak/T) - 1)
 
-	return nk * omegak * V
+    return nk * omegak * V
 
 
 def thermal(shape,
-			eta0, 
-			gstar, 
-			fa,
-			scale,
-			kmax=10):
-	'''
-	generate initial field (and first derivative) values with thermal distribution 
+            eta0, 
+            gstar, 
+            fa,
+            scale,
+            kmax=10):
+    '''
+    generate initial field (and first derivative) values with thermal distribution 
 
-	shape: spatial grid size, e.g. 20 * 20 for 2D or 20 * 20 * 20 for 3D
-	eta0: tilde eta at which you want to initialize thermally
-	gstar: enters functions of scale factor and temperature and so on
-	fa: axion decay constant
-	scale: L_physical / L_lattice
-	kmax: half of the box size of k-modes summed over in Fourier Transform
-	'''
-	assert type(shape) is tuple
+    shape: spatial grid size, e.g. 20 * 20 for 2D or 20 * 20 * 20 for 3D
+    eta0: tilde eta at which you want to initialize thermally
+    gstar: enters functions of scale factor and temperature and so on
+    fa: axion decay constant
+    scale: L_physical / L_lattice
+    kmax: half of the box size of k-modes summed over in Fourier Transform
+    '''
+    assert type(shape) is tuple
 
-	R1, T1, t1 = init_PQ_params(fa,gstar)
-	T0 = T(gstar,t(eta0, t1))
-	H0 = H(gstar,T0)
-	R0 = R(gstar,T0)
-
-
-	dim = len(shape)  # spatial dimensions of lattice
-	Lx, Ly, Lz = shape
-	Lx_phys = Lx * scale / (R0 * H0)
-	Ly_phys = Ly * scale / (R0 * H0)
-	Lz_phys = Lz * scale / (R0 * H0)
-	V = Lx_phys * Ly_phys * Lz_phys
-
-	# initialize arrays
-	phi1_k_lattice = np.zeros(shape)
-	phi2_k_lattice = np.zeros(shape)
-	phi1dot_k_lattice = np.zeros(shape)
-	phi2dot_k_lattice = np.zeros(shape)
-
-	# fill whole array with Gaussian dist
-	for x in range(Lx):
-		# zero momentum is at index int(Lx/2)
-		kx = (x - int(Lx/2)) * scale / (R0 * H0)
-		for y in range (Ly):
-			ky =  (y - int(Ly/2)) * scale / (R0 * H0)
-			for z in range(Lz):
-				kz = (z - int(Lz/2)) * scale / (R0 * H0)
-				ksq = kx^2 + ky^2 + kz^2
-				phi1_k_lattice[x,y,z], phi2_k_lattice[x,y,z] = np.random.normal(0.,stand_dev_phi(ksq,V,T0,fa))
-				phi1dot_k_lattice[x,y,z], phi2dot_k_lattice[x,y,z] =np.random.normal(0.,stand_dev_phidot(ksq,V,T0,fa))
+    R1, T1, t1 = init_PQ_params(fa,gstar)
+    T0 = T(gstar,t(eta0, t1))
+    H0 = H(gstar,T0)
+    R0 = R(gstar,T0)
 
 
-	# mask out with zeros
-	# wants to keep int(Lx/2) - kmax to int(Lx/2) + kmax
-	phi1_k_lattice[:int(Lx/2)-kmax,:,:] = 0
-	phi1_k_lattice[int(Lx/2)+kmax:,:,:] = 0
-	phi2_k_lattice[:int(Lx/2)-kmax,:,:] = 0
-	phi2_k_lattice[int(Lx/2)+kmax:,:,:] = 0
-	phi1_k_lattice[:,:int(Ly/2)-kmax,:] = 0
-	phi1_k_lattice[:,int(Ly/2)+kmax:,:] = 0
-	phi2_k_lattice[:,:int(Ly/2)-kmax,:] = 0
-	phi2_k_lattice[:,int(Ly/2)+kmax:,:] = 0	
-	phi1_k_lattice[:,:,:int(Lz/2)-kmax] = 0
-	phi1_k_lattice[:,:,int(Lz/2)+kmax:] = 0
-	phi2_k_lattice[:,:,:int(Lz/2)-kmax] = 0
-	phi2_k_lattice[:,:,int(Lz/2)+kmax:] = 0
+    dim = len(shape)  # spatial dimensions of lattice
+    Lx, Ly, Lz = shape
+    Lx_phys = Lx * scale / (R0 * H0)
+    Ly_phys = Ly * scale / (R0 * H0)
+    Lz_phys = Lz * scale / (R0 * H0)
+    V = Lx_phys * Ly_phys * Lz_phys
+
+    # initialize arrays
+    phi1_k_lattice = np.zeros(shape)
+    phi2_k_lattice = np.zeros(shape)
+    phi1dot_k_lattice = np.zeros(shape)
+    phi2dot_k_lattice = np.zeros(shape)
+
+    # fill whole array with Gaussian dist
+    for x in range(Lx):
+        # zero momentum is at index int(Lx/2)
+        kx = (x - int(Lx/2)) * scale / (R0 * H0)
+        for y in range (Ly):
+            ky =  (y - int(Ly/2)) * scale / (R0 * H0)
+            for z in range(Lz):
+                kz = (z - int(Lz/2)) * scale / (R0 * H0)
+                ksq = kx**2 + ky**2 + kz**2
+                phi1_k_lattice[x,y,z], phi2_k_lattice[x,y,z] = np.random.normal(0.,stand_dev_phi(ksq,V,T0,fa),2)
+                phi1dot_k_lattice[x,y,z], phi2dot_k_lattice[x,y,z] =np.random.normal(0.,stand_dev_phidot(ksq,V,T0,fa),2)
 
 
-	# Inverse Fourier Transform!
-	phi1_x_lattice = np.fft.irfftn(phi1_k_lattice,shape)
-	phi2_x_lattice = np.fft.irfftn(phi2_k_lattice,shape)
-	phi1dot_x_lattice = np.fft.irfftn(phi1dot_k_lattice,shape)
-	phi2dot_x_lattice = np.fft.irfftn(phi2dot_k_lattice,shape)
+    # mask out with zeros
+    # wants to keep int(Lx/2) - kmax to int(Lx/2) + kmax
+    phi1_k_lattice[:int(Lx/2)-kmax,:,:] = 0
+    phi1_k_lattice[int(Lx/2)+kmax:,:,:] = 0
+    phi2_k_lattice[:int(Lx/2)-kmax,:,:] = 0
+    phi2_k_lattice[int(Lx/2)+kmax:,:,:] = 0
+    phi1_k_lattice[:,:int(Ly/2)-kmax,:] = 0
+    phi1_k_lattice[:,int(Ly/2)+kmax:,:] = 0
+    phi2_k_lattice[:,:int(Ly/2)-kmax,:] = 0
+    phi2_k_lattice[:,int(Ly/2)+kmax:,:] = 0 
+    phi1_k_lattice[:,:,:int(Lz/2)-kmax] = 0
+    phi1_k_lattice[:,:,int(Lz/2)+kmax:] = 0
+    phi2_k_lattice[:,:,:int(Lz/2)-kmax] = 0
+    phi2_k_lattice[:,:,int(Lz/2)+kmax:] = 0
 
 
-	# return psi not phi fields
-	# including jacobian factor from dt to d eta
-	jac = 2 * sqrt(t(eta0, t1)/ t1)
-	return phi1_x_lattice / fa, phi2_x_lattice / fa, jac * phi1dot_x_lattice / fa, jac * phi2dot_x_lattice / fa
+    phi1dot_k_lattice[:int(Lx/2)-kmax,:,:] = 0
+    phi1dot_k_lattice[int(Lx/2)+kmax:,:,:] = 0
+    phi2dot_k_lattice[:int(Lx/2)-kmax,:,:] = 0
+    phi2dot_k_lattice[int(Lx/2)+kmax:,:,:] = 0
+    phi1dot_k_lattice[:,:int(Ly/2)-kmax,:] = 0
+    phi1dot_k_lattice[:,int(Ly/2)+kmax:,:] = 0
+    phi2dot_k_lattice[:,:int(Ly/2)-kmax,:] = 0
+    phi2dot_k_lattice[:,int(Ly/2)+kmax:,:] = 0
+    phi1dot_k_lattice[:,:,:int(Lz/2)-kmax] = 0
+    phi1dot_k_lattice[:,:,int(Lz/2)+kmax:] = 0
+    phi2dot_k_lattice[:,:,:int(Lz/2)-kmax] = 0
+    phi2dot_k_lattice[:,:,int(Lz/2)+kmax:] = 0
+
+
+    # rolling in the deep
+    phi1_k_lattice = np.roll(phi1_k_lattice,(-int(Lx/2),-int(Ly/2),-int(Lz/2)),axis=(0,1,2))
+    phi2_k_lattice = np.roll(phi2_k_lattice,(-int(Lx/2),-int(Ly/2),-int(Lz/2)),axis=(0,1,2))
+    phi1dot_k_lattice = np.roll(phi1dot_k_lattice,(-int(Lx/2),-int(Ly/2),-int(Lz/2)),axis=(0,1,2))
+    phi2dot_k_lattice = np.roll(phi2dot_k_lattice,(-int(Lx/2),-int(Ly/2),-int(Lz/2)),axis=(0,1,2))
+
+
+    # Inverse Fourier Transform!
+    phi1_x_lattice = np.fft.irfftn(phi1_k_lattice,shape)
+    phi2_x_lattice = np.fft.irfftn(phi2_k_lattice,shape)
+    phi1dot_x_lattice = np.fft.irfftn(phi1dot_k_lattice,shape)
+    phi2dot_x_lattice = np.fft.irfftn(phi2dot_k_lattice,shape)
+
+
+    # return psi not phi fields
+    # including jacobian factor from dt to d eta
+    jac = 2 * np.sqrt(t(eta0, t1)/ t1) * t1
+    return phi1_x_lattice / fa, phi2_x_lattice / fa, jac * phi1dot_x_lattice / fa, jac * phi2dot_x_lattice / fa
 
 
 
 
 
-	
+    
 
 
